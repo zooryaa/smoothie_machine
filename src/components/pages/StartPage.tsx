74d0f7ba4dc16bce05e710985cd1e2aa9705e2bf
sophia <sophia.moos@noseryoung.com>
2023-05-07 15:26:52 +0200

    added styling

--- src/components/pages/StartPage.tsx
+++ src/components/pages/StartPage.tsx
@@ -1,12 +1,18 @@
-import { Button } from '@mui/material'
+import {Button, Box, Typography} from '@mui/material'
 import { useNavigate } from 'react-router-dom'
+import React from "react";
 
 function StartPage() {
 
   const navigate = useNavigate();
 
   return (
-    <Button onClick={()=>{navigate("/form")}}>Start</Button>
+      <Box sx={{height: "100vh", width: "100vw", background: "linear-gradient(180deg, rgba(190,218,16,1) 0%, rgba(84,228,10,1) 42%, rgba(0,255,222,1) 100%)", alignItems: "center", justifyContent: "center", overflow: "hidden"}}>
+        <Box sx={{height: "50vh", width: "40vw", ml: "30vw", mr: "30vw", mt: "25vh", mb: "25vh"}}>
+          <Typography sx={{fontSize: 32, color: "#fff", fontWeight: 600}}>SMOOTHIE MIXER</Typography>
+          <Button onClick={()=>{navigate("/form")}} variant="contained">Create your smoothie</Button>
+        </Box>
+      </Box>
   )
 }
 
